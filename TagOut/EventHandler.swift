d4797bc7bc55875d65e76299c07de1f51b460ece
Maxim Kuzmenko <maximkuzmenko@Maxims-MacBook-Pro.local>
2017-04-07 02:36:22 -0400

    Created Project

---
+++ TagOut/EventHandler.swift
@@ -0,0 +1,9 @@
+//
+//  EventHandler.swift
+//  TagOut
+//
+//  Created by Maxim Kuzmenko on 2017-04-07.
+//  Copyright © 2017 Maxim Kuzmenko. All rights reserved.
+//
+
+import Foundation
--- TagOut/RoomViewController.swift
+++ TagOut/RoomViewController.swift
@@ -12,7 +12,6 @@ class DetailViewController: UIViewController {
 
     @IBOutlet weak var detailDescriptionLabel: UILabel!
 
-
     func configureView() {
         // Update the user interface for the detail item.
         if let detail = self.detailItem {
@@ -33,7 +32,7 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    var detailItem: NSDate? {
+    var detailItem: String? {
         didSet {
             // Update the view.
             self.configureView()
